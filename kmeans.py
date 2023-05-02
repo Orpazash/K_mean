77ae32d00f15cf23c92b9e4ec81e3f4766bdb479
orkatzz <orkatz@mail.tau.ac.il>
2023-05-02 13:04:14 +0300

    first edit wow

--- kmeans.py
+++ kmeans.py
@@ -1,1 +1,15 @@
+def compute_distance(dot1, dot2):
+    return None
+
+def check_delta_cenroids(old_centroids, new_centroids):
+    return None
+
+def compute_centroid(cluster_array):
+    return None
+
+# remember try and catch
+# remember do while
+# 4 points after the dot at print
+#
+
 
