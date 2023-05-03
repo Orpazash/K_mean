2466c1ac3f19684592e53ed559258f027a1f7cef
orpazash <orpaza@mail.tau.ac.il>
2023-05-03 09:58:02 +0300

    test

--- kmeans.c
+++ kmeans.c
@@ -1,1 +1,12 @@
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+    char name[5];
+    printf("enter your name: ");
+    scanf("%s", name);
+    printf("your name is %s", name);
+    return 0;
+}
 
